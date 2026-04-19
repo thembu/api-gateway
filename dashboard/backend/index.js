f886274ad959405241dd4d807031982fb6e06471
thembu <rakhivhanithembu@gmail.com>
2026-04-19 14:05:20 +0200

    edited path to endpoint

--- dashboard/backend/index.js
+++ dashboard/backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/stats', statsRoute);
+app.use('/api/stats', statsRoute);
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
