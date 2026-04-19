b59632051d8f5c82df9bb3a8bbab1285b83f314a
thembu <rakhivhanithembu@gmail.com>
2026-04-19 13:10:47 +0200

    added logger to log requests

--- gateway/index.js
+++ gateway/index.js
@@ -2,8 +2,14 @@ const http = require('http');
 const proxy = require('./proxy')
 const rateLimit = require('./middleware/ratelimit')
 const logger = require('./middleware/logger')
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3001;
+
+const server = http.createServer( async (req , res) => {
+
+   const start = Date.now();
+   
 
 //capture ip address for rate limiting and logging
 const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
@@ -15,20 +21,23 @@ const allowed = await rateLimit(ip)
 if(!allowed) {
     res.writeHead(429 , {'Content-Type' : 'application/json'})
     res.end(JSON.stringify({ error: 'Too many requests' }))
+
+    const duration = Date.now() - start;
+
     //log the blocked request
-    await logger({method : req.method ,  path : req.path , status : 429 , ip,blocked : true})
+    await logger({method : req.method ,  path : req.path , status : 429 , ip,blocked : true, duration})
     return
 }
 
 //forward the request to the appropriate microservice and log the response status code  
 
 proxy(req , res , async (statusCode) => {
-    await logger({method : req.method ,  path : req.path , status : statusCode , ip,blocked : false})   
+    const duration = Date.now() - start;
+    await logger({method : req.method ,  path : req.path , status : statusCode , ip,blocked : false, duration})   
 
 })
 
-const server = http.createServer( async (req , res) => {})
-
+})
 server.listen(PORT, () => {
     console.log(`API Gateway running on port ${PORT}`);
 })
---
+++ gateway/middleware/logger.js
@@ -0,0 +1,16 @@
+const pool = require("../db");
+
+module.exports = async function logger({ method, path, status, ip, blocked }) {
+  try {
+
+    await pool.execute(
+      `INSERT INTO request_logs 
+        (method, path, status_code, duration_ms, ip, blocked, block_reason) 
+       VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      [method, path, status, duration, ip, blocked, blockReason]
+    );    
+
+  } catch (err) {
+    console.error("Logging error:", err);
+  }
+};
